8e01f8c6ec0fcfabfa47a992febdf674820e464c
namdoiz <nnamdi.on7@gmail.com>
2022-02-14 20:31:15 -0600

    A method that takes an array argument of integers and returns the average

---
+++ array_average.rb
@@ -0,0 +1,20 @@
+=begin
+
+Write a method that takes one argument, 
+an array containing integers, and returns the 
+average of all numbers in the array. The array 
+will never be empty and the numbers will always 
+be positive integers. Your result should also be 
+an integer.
+
+=end
+
+def average (arr)
+  x = arr.sum
+  y = arr.count
+  b = x / y 
+end
+
+puts average([1, 6]) == 3 
+puts average([1, 5, 87, 45, 8, 8]) == 25
+puts average([9, 47, 23, 95, 16, 52]) == 40
