8a756d8ab910591b6a5dbc01c9c66685518b2bae
namdoiz <nnamdi.on7@gmail.com>
2022-02-15 20:31:09 -0600

    using .rand with puts

---
+++ how_old_is_teddy.rb
@@ -0,0 +1,10 @@
+=begin
+
+Build a program that randomly generates and 
+prints Teddy's age. To get the age, you should 
+generate a random number between 20 and 200.
+
+=end
+
+puts "Teddy is #{rand(20...200)} years old!"
+
