d5801cd8240a158a1f6e17b911ee310fad6ca114
namdoiz <nnamdi.on7@gmail.com>
2022-03-17 00:59:32 -0500

    a method that takes an Array of numbers and then returns the sum of the sums of each leading subsequence for that Array

---
+++ sum_of_sums.rb
@@ -0,0 +1,22 @@
+=begin
+
+Write a method that takes an Array of numbers and 
+then returns the sum of the sums of each leading 
+subsequence for that Array. You may assume that the 
+Array always contains at least one number.
+
+=end
+
+def sum_of_sums(arr1)
+  arr2 = []
+  loop do
+    arr2 << arr1.sum
+    arr1.pop
+    break if arr1.empty?
+  end
+  arr2.inject(:+)
+end
+p sum_of_sums([3, 5, 2]) == (3) + (3 + 5) + (3 + 5 + 2) # -> (21)
+p sum_of_sums([1, 5, 7, 3]) == (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) # -> (36)
+p sum_of_sums([4]) == 4
+p sum_of_sums([1, 2, 3, 4, 5]) == 35
