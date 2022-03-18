a793d69b59e1dc41450860735f208827a6f9111f
namdoiz <nnamdi.on7@gmail.com>
2022-03-18 15:19:53 -0500

    a method that returns the negative of whatever number given by the user

---
+++ always_return_negative.rb
@@ -0,0 +1,16 @@
+=begin
+
+Write a method that takes a number as an argument. 
+If the argument is a positive number, return the negative 
+of that number. If the number is 0 or negative, 
+return the original number.
+
+=end
+
+def negative(int)
+  int > 0 ? -int : int
+end
+
+p negative(5) == -5
+p negative(-3) == -3
+p negative(0) == 0      # There's no such thing as -0 in ruby
